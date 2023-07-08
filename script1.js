90a3108c563242ac1b0edcb67eeac758991c8402
Kabwe Zacharia Chanda <65198337+mwapkabq@users.noreply.github.com>
2023-07-08 23:19:08 +0200

    Update script1.js

---
+++ script1.js
@@ -0,0 +1,22 @@
+The following is the response to the assignment regarding simple interest.
+  Simple interest is a basic form of interest that is calculated on the initial principal amount over a certain period of time. It does not take into account the compounding of interest. The formula for calculating simple interest is:
+
+Simple Interest = Principal Amount × Interest Rate × Time
+
+where:
+
+Principal Amount is the initial amount of money or the principal sum.
+Interest Rate is the percentage rate at which interest is charged or earned.
+Time is the duration for which the interest is calculated, typically in years.
+The simple interest formula calculates the total amount of interest earned or paid. To calculate the final amount including the principal and interest, you would add the simple interest to the principal.
+
+Here's an example:
+
+Let's say you have $1,000 as the principal amount, an interest rate of 5% per year, and a time period of 2 years.
+
+Using the formula:
+Simple Interest = $1,000 × 0.05 × 2 = $100
+
+The simple interest earned over the 2-year period is $100. So, the final amount after 2 years would be $1,100 ($1,000 principal + $100 interest).
+
+It's important to note that simple interest does not account for compounding, where the interest is added back to the principal periodically. Simple interest is commonly used in simple financial calculations, such as short-term loans or simple investment scenarios.
